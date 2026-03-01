86a7d67d09873fb9fdf43d74d7cdf8c37eb7b788
Константин Крючков <evilfoxmine@gmail.com>
2026-03-01 14:09:12 +0700

    Create sorters

---
+++ src/AssignmentSorters.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+
+public final class AssignmentSorters {
+
+    private AssignmentSorters() {}
+
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(a -> a.getUser().getUsername());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(a -> a.getRole().getName());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(RoleAssignment::getAssignedAt);
+    }
+}
---
+++ src/UserSorters.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+
+public final class UserSorters {
+
+    private UserSorters() {}
+
+    public static Comparator<User> byUsername() {
+        return Comparator.comparing(User::getUsername);
+    }
+
+    public static Comparator<User> byFullName() {
+        return Comparator.comparing(User::getFullName);
+    }
+
+    public static Comparator<User> byEmail() {
+        return Comparator.comparing(User::getEmail);
+    }
+}
